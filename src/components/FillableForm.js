8bde5b0ccd9d90106dd14d70b2c4499ef1f7d361
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-06-14 15:50:40 -0800

    back to basics

--- src/components/FillableForm.js
+++ src/components/FillableForm.js
@@ -9,13 +9,13 @@ const FillableForm = (props) => {
         <label for = "goal">Tell me about one improvement your would like to see in yourself, in the next three months (as a result of our time together): <input placeholder= 'Enter Your Goal Here' type='string'/></label><br></br><br></br>
 
         <br></br><br></br>
-        <label for = "frequency">How often do you need to meet? (Sometimes your Insurance Provider will have some input)</label>
+        {/* <label for = "frequency">How often do you need to meet? (Sometimes your Insurance Provider will have some input)</label>
             <select name= "frequency" id="frequency">
                 <option value="One time weekly">Once Weekly</option> 
                 <option value="Twice weekly">Twice Weekly</option> 
                 <option value="Every Two weeks">Every Two Weeks</option> 
                 <option value="Once Monthly">Once Monthly</option> 
-            </select>
+            </select> */}
             <br></br><br></br>
             <label for = "coping">What are some things you do that help you feel better when you are having a tough time?: <input placeholder= 'Coping Skills' type='string'/></label><br></br><br></br>
             <label for = "contacts">Who can you call if you need somthing?: <input placeholder= 'Supportive Contacts' type='string'/></label><br></br><br></br>
