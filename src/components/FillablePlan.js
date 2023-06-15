d2862c70bd21560d7c4cd0858230d4c4e9ed12cc
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-06-15 07:14:36 -0800

    fixing for error to htmlFor

--- src/components/FillablePlan.js
+++ src/components/FillablePlan.js
@@ -3,13 +3,13 @@ import React from 'react';
 const FillablePlan = (props) => {
     return(
         <div className = 'fillablePlan'>
-        <label for = "title"><h1>Treatment Plan</h1></label><br></br><br></br>
+        <label htmlFor = "title"><h1>Treatment Plan</h1></label><br></br><br></br>
         <h2>Let's Make Your Plan</h2>
-        <label for = "name">Let's Start with your name: <input placeholder= 'Enter Your First and Last Name' type='string'/></label><br></br><br></br>
-        <label for = "goal">Tell me about one improvement your would like to see in yourself, in the next three months (as a result of our time together): <input placeholder= 'Enter Your Goal Here' type='string'/></label><br></br><br></br>
+        <label htmlFor = "name">Let's Start with your name: <input placeholder= 'Enter Your First and Last Name' type='string'/></label><br></br><br></br>
+        <label htmlFor = "goal">Tell me about one improvement your would like to see in yourself, in the next three months (as a result of our time together): <input placeholder= 'Enter Your Goal Here' type='string'/></label><br></br><br></br>
 
         <br></br><br></br>
-        <label for = "frequency">How often do you need to meet? (Sometimes your Insurance Provider will have some input)</label>
+        <label htmlFor = "frequency">How often do you need to meet? (Sometimes your Insurance Provider will have some input)</label>
             <select name= "frequency" id="frequency">
                 <option value="One time weekly">Once Weekly</option> 
                 <option value="Twice weekly">Twice Weekly</option> 
@@ -17,8 +17,8 @@ const FillablePlan = (props) => {
                 <option value="Once Monthly">Once Monthly</option> 
             </select>
             <br></br><br></br>
-            <label for = "coping">What are some things you do that help you feel better when you are having a tough time?: <input placeholder= 'Coping Skills' type='string'/></label><br></br><br></br>
-            <label for = "contacts">Who can you call if you need somthing?: <input placeholder= 'Supportive Contacts' type='string'/></label><br></br><br></br>
+            <label htmlFor = "coping">What are some things you do that help you feel better when you are having a tough time?: <input placeholder= 'Coping Skills' type='string'/></label><br></br><br></br>
+            <label htmlFor = "contacts">Who can you call if you need somthing?: <input placeholder= 'Supportive Contacts' type='string'/></label><br></br><br></br>
             <br></br>
     </div>
     
--- src/components/SafetyPlan.js
+++ src/components/SafetyPlan.js
@@ -7,10 +7,10 @@ const SafetyPlan = (props) => {
         <h1>Safety Plan</h1>
         <h2>What can be done to help you stay safe?</h2>
         <h3>If you are having self-harm thoughts, you can reach out to the National Suicide Hotline at 988 </h3>
-        <label for = "crisis">Who else could you call if you were having suicidal thoughts? <input placeholder= 'Enter Name & Number' type='string'/></label><br></br><br></br>
-        <label for = "coping">What can you do to manage your emotions or distract yourself? <input placeholder= 'Add Coping Skills' type='string'/></label><br></br><br></br>
-        <label for = "provider">Do you have a provider we should call? <input placeholder= 'Enter Name & Number' type='string'/></label><br></br><br></br>
-        <label for = "petsKids">Do you have pets or kids we need to help make arrangements for (if you are hospitalized)? <input placeholder= 'Enter Info' type='string'/></label><br></br><br></br>
+        <label htmlFor = "crisis">Who else could you call if you were having suicidal thoughts? <input placeholder= 'Enter Name & Number' type='string'/></label><br></br><br></br>
+        <label htmlFor = "coping">What can you do to manage your emotions or distract yourself? <input placeholder= 'Add Coping Skills' type='string'/></label><br></br><br></br>
+        <label htmlFor = "provider">Do you have a provider we should call? <input placeholder= 'Enter Name & Number' type='string'/></label><br></br><br></br>
+        <label htmlFor = "petsKids">Do you have pets or kids we need to help make arrangements for (if you are hospitalized)? <input placeholder= 'Enter Info' type='string'/></label><br></br><br></br>
         </div>
     );
 };
